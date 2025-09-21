3b883cb29e3aaf0bc0addaaec0580111e66881aa
Rama Herbin <ramacharles.h@gmail.com>
2025-09-21 12:53:05 -0500

    temporary publications plus content

---
+++ content/research/publications-plus/publicationsPlusContent.js
@@ -0,0 +1,160 @@
+
+
+const publicationsPlus = [
+  {
+    "title": "Finite volume methods",
+    "authors": "Eymard, Robert; Gallouët, Thierry; Herbin, Raphaèle",
+    "year": "2000",
+    "source": "North-Holland,Amsterdam, 2000, 713–1020",
+    "doi": "10.1016/S1570-8659(00)07005-8",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1804748",
+    "citations": 1029
+  },
+  {
+    "title": "AnL1-theory of existence and uniqueness of solutions of nonlinear elliptic equations",
+    "authors": "Bénilan, Philippe; Boccardo, Lucio; Gallouët, Thierry; Gariepy, Ron; Pierre, Michel; Vázquez, Juan Luis",
+    "year": "1995",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1354907",
+    "citations": 797
+  },
+  {
+    "title": "Some results for L1 nonlinear parabolic equations",
+    "authors": "Boccardo, L.; Gallouet, T.",
+    "year": "1989",
+    "doi": "10.1016/0362-546X(89)90036-0",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=0997579",
+    "citations": 479
+  },
+  {
+    "title": "Nonlinear elliptic equations in L1",
+    "authors": "Boccardo, L.; Gallouet, T.",
+    "year": "1980",
+    "doi": "10.1016/0362-546X(80)90068-1",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=0582536",
+    "citations": 386
+  },
+  {
+    "title": "A unified approach to mimetic finite difference, hybrid finite volume and mixed finite volume methods",
+    "authors": "Droniou, Jérôme; Eymard, Robert; Gallouët, Thierry; Herbin, Raphaèle",
+    "year": "2010",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=2649153",
+    "citations": 295
+  },
+  {
+    "title": "Nonlinear parabolic equations with measure data",
+    "authors": "Boccardo, Lucio; Dall'Aglio, Andrea; Gallouët, Thierry; Orsina, Luigi",
+    "year": "1997",
+    "doi": "10.1006/jfan.1996.3040",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1453181",
+    "citations": 259
+  },
+  {
+    "title": "Nonlinear elliptic equations in L1∩W−1,1",
+    "authors": "Boccardo, L.; Gallouët, T.",
+    "year": "1989",
+    "doi": "10.1007/BF01331736",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1007334",
+    "citations": 258
+  },
+  {
+    "title": "On the best constant in the Moser–Trudinger inequality",
+    "authors": "Aubin, Thierry; Brézis, H.; Gallouët, T.",
+    "year": "1979",
+    "doi": "10.1016/0022-247X(79)90076-9",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=0544733",
+    "citations": 238
+  },
+  {
+    "title": "The gradient discretisation method",
+    "authors": "Droniou, Jérôme; Eymard, Robert; Gallouët, Thierry; Guichard, Cindy; Herbin, Raphaèle",
+    "year": "2018",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=3898702",
+    "citations": 147
+  },
+  {
+    "title": "A remark on L∞ estimates for nonlinear elliptic equations",
+    "authors": "Brezis, H.; Gallouet, T.",
+    "year": "1980",
+    "doi": "10.1016/0362-546X(80)90046-2",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=0580765",
+    "citations": 144
+  },
+  {
+    "title": "On the stability of finite volume schemes for convection-diffusion equations",
+    "authors": "Eymard, Robert; Gallouët, Thierry; Herbin, Raphaèle",
+    "year": "2000",
+    "doi": "10.1051/m2an:2000109",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1775220",
+    "citations": 135
+  },
+  {
+    "title": "A convergent finite-element method for fully nonlinear second-order equations",
+    "authors": "Feng, Xiaobing; Jensen, Max; Smears, Ian",
+    "year": "2015",
+    "doi": "10.1090/S0025-5718-2014-02892-3",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=3322270",
+    "citations": 132
+  },
+  {
+    "title": "A variant of the Brezis–Merle inequality and application to elliptic equations involving exponential nonlinearities",
+    "authors": "Adimurthi; Druet, Olivier",
+    "year": "2004",
+    "doi": "10.1112/S0024611503014601",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=2049019",
+    "citations": 117
+  },
+  {
+    "title": "On entropy solutions for nonlinear elliptic equations with measure data",
+    "authors": "Gallouët, Thierry; Morel, Jacques-Michel",
+    "year": "1988",
+    "doi": "10.1016/0022-247X(88)90161-7",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=0956043",
+    "citations": 107
+  },
+  {
+    "title": "Existence and uniqueness of entropy solutions for nonlinear elliptic equations with measure data",
+    "authors": "Gallouët, Thierry; Morel, Jacques-Michel",
+    "year": "1997",
+    "doi": "10.1016/S0764-4442(97)87004-4",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1409661",
+    "citations": 106
+  },
+  {
+    "title": "On the boundedness by entropy of the L1 solutions of nonlinear elliptic equations",
+    "authors": "Boccardo, L.; Gallouët, T.",
+    "year": "1992",
+    "doi": "10.1016/0362-546X(92)90242-C",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1185324",
+    "citations": 102
+  },
+  {
+    "title": "Existence and nonexistence of solutions for some nonlinear elliptic equations",
+    "authors": "Boccardo, Lucio; Gallouët, Thierry; Orsina, Luigi",
+    "year": "1997",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1616410",
+    "citations": 100
+  },
+  {
+    "title": "Some convergent finite volume schemes for Hamilton–Jacobi equations",
+    "authors": "Falcone, Maurizio; Ferretti, Roberto; Gallouët, Thierry; Vénéreau, Alexis",
+    "year": "2004",
+    "doi": "10.1051/m2an:2004016",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=2050136",
+    "citations": 88
+  },
+  {
+    "title": "Existence and uniqueness of solutions for some nonlinear elliptic equations",
+    "authors": "Boccardo, Lucio; Gallouët, Thierry; Orsina, Luigi",
+    "year": "1996",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=1364043",
+    "citations": 87
+  },
+  {
+    "title": "Asymptotic behavior of solutions for nonlinear parabolic equations involving entropy",
+    "authors": "Boccardo, L.; Gallouët, T.",
+    "year": "1987",
+    "doi": "10.1016/0022-247X(87)90042-6",
+    "url": "https://mathscinet-ams-org.ezproxy.math.cnrs.fr/mathscinet/article?mr=0890450",
+    "citations": 73
+  }
+];
