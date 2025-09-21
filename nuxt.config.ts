32c2c87210cd1f476ca89ed7b77278e5d908dcd1
Rama Herbin <ramacharles.h@gmail.com>
2025-09-21 12:42:24 -0500

    refactor(config): format/syntax tweaks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: '',
+    prefix: "",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './app/components/ui'
+    componentDir: "./app/components/ui",
   },
-  css: ['~/assets/css/tailwind.css'],
+  css: ["~/assets/css/tailwind.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-  modules: ['shadcn-nuxt', '@nuxt/content', '@nuxthq/studio'],
+  modules: ["shadcn-nuxt", "@nuxt/content", "@nuxthq/studio"],
   content: {
-    // Configuration pour les collections de contenu
+    preview: {
+      api: "https://api.nuxt.studio",
+    },
     sources: {
       content: {
-        driver: 'fs',
-        prefix: '/content', // URL prefix
-        base: './content' // Path source
-      }
-    }
+        driver: "fs",
+        prefix: "/content", // URL prefix
+        base: "./content", // Path source
+      },
+    },
   },
-})
+});
