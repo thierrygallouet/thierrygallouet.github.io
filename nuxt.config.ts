c8a7a60c72316349f3e978844f950403551a21b9
Rama Herbin <ramacharles.h@gmail.com>
2025-11-24 16:52:29 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,6 @@ export default defineNuxtConfig({
     baseURL: '/', // Base URL pour GitHub Pages
     buildAssetsDir: '/assets/', // Dossier des assets
   },
-  ssr: true, // Activer SSR pour le pre-rendering
   experimental: {
     payloadExtraction: false // Désactiver l'extraction de payload pour éviter les problèmes d'hydratation
   },
@@ -29,7 +28,6 @@ export default defineNuxtConfig({
      */
     componentDir: "./app/components/ui",
   },
-  // css: ["./app/assets/css/tailwind.css"],
   vite: {
     plugins: [tailwindcss()],
   },
